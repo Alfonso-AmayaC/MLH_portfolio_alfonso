bdf8cfc15f9d1919525c64594f7f121788ba21eb
Alfonso Amaya <alfonsocamargo0108@gmail.com>
2023-07-29 06:06:42 +0000

    Update redeploy site

--- redeploy-website.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-systemctl stop myportfolio
-cd /home/MLH_portfolio_alfonso
-git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate && pip install -r requirements.txt && deactivate
-systemctl restart myportfolio
